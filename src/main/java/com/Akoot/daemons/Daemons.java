5b668e020d1e04a09189da1e46d79f8cf5dd560f
Akoot <akoot311@gmail.com>
2016-06-28 15:03:24 -0400

    init commit
    Imported from empty-spigot-plugin

---
+++ src/main/java/com/Akoot/daemons/Daemons.java
@@ -0,0 +1,16 @@
+package com.Akoot.daemons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Daemons extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+	}
+	
+	@Override
+	public void onDisable()
+	{
+	}
+}
